62c7d4326795ded1faee9029cfd8ac4238fb875d
Asele <none@nothing.com>
2014-06-03 19:13:27 +0200

    Hubert? : )

---
+++ loop_7.sh
@@ -0,0 +1,39 @@
+#!/bin/bash -x
+
+# Write a script that will create a directory which will contain data
+# which will be shared across members of some project group.
+
+ANS="-a"
+
+if [ $1 == "-"? ] ; then
+    ANS=$1
+    shift
+fi
+
+DIR=$1
+shift
+
+if [ ! -d "$DIR" ] ; then
+    mkdir "$DIR"
+fi
+
+case "$ANS" in
+    "-a") 
+        while [ -n "$1" ] ; do
+            setfacl -Rm u:"$1":rw -- "$DIR"
+            setfacl -d -Rm u:"$1":rw -- "$DIR"
+            shift
+        done 
+        ;;
+    "-r")
+        while [ -n "$1" ] ; do
+            setfacl -Rm u:"$1" -- "$DIR"
+            setfacl -d -Rx u:"$1" -- "$DIR"
+            shift
+        done
+        ;;
+esac
+    
+
+
+
